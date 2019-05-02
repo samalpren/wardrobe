33143664cf581e5f223cf8934ef0e266749e078f
Sam Alpren <samalpren1@gmail.com>
2019-05-02 10:36:10 -0500

    added items to database and began working on login

---
+++ src/scripts/dom-manager.js
@@ -0,0 +1,25 @@
+buildLoginForm() {
+
+  const loginContainer = document.querySelector("users-login-section");
+
+  clearElement(loginContainer);
+
+  const userName = document.createElement("label");
+  userNameLabel.textContent = "User name: ";
+  const userNameInput = document.createElement("Input");
+  userNameInput.id = "users-login-section";
+
+  const nameLabel = document.createElement("label");
+  nameLabel.textContent = "Name: ";
+  const nameInput = document.createElement("input");
+  nameInput.id = "name-title-input";
+
+  const emailLabel = document.createElement("label");
+  emailLabel.textContent = "Email: ";
+  const emailInput = document.createElement("input");
+  emailInput.id = "email-input";
+
+  const saveUser = document.createElement("button");
+  saveUser.textContent = "Sign Up!";
+  saveUser.id = "saveUser";
+}
